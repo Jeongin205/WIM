433a63aa66afd4b9109edbe991eb9af73fc14bdc
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-10-10 01:15:50 +0900

    기본 로그인 구조 구현

---
+++ app/src/main/java/com/example/wim/activity/LoginActivity.kt
@@ -0,0 +1,59 @@
+package com.example.wim.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.wim.R
+import com.example.wim.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+    private val TAG : String = "LoginActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
+
+        binding.signUpButton.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.loginButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+            if (email.isBlank() && password.isBlank()) Toast.makeText(
+                this,
+                "빈칸을 채워주세요",
+                Toast.LENGTH_SHORT
+            )
+                .show()
+            else if (email.isBlank()) Toast.makeText(this, "이메일을 입력해주세요", Toast.LENGTH_SHORT)
+                .show()
+            else if (password.isBlank()) Toast.makeText(this, "비밀번호를 입력해주세요", Toast.LENGTH_SHORT)
+                .show()
+            else {
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            Log.d(TAG, "signInWithEmail:success")
+                            val user = auth.currentUser
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            Log.w(TAG, "signInWithEmail:failure", task.exception)
+                            Toast.makeText(this, "사용자 정보를 확인해 주세요", Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+            }
+
+        }
+    }
+}
